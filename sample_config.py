e1f60f872e449aaf43ecdc7ec23a2cf054d1ede0
kit567 <kit567@yandex.ru>
2021-01-18 09:30:33 +0300

    change /

--- sample_config.py
+++ sample_config.py
@@ -3,5 +3,5 @@ user = 'ftpusername'
 passwd = 'ftppassword'
 basepath = '/path/to/backup/folder'  # Папка, в которой будут созданы подпапки со скачанными файлами
 max_threads = 20 # максимальное количество одновременных процессов загрузки
-log_path = '\path\to\logfile'
-statusfilepath = '\path\to\statusfile'
+log_path = '/path/to/logfile'
+statusfilepath = '/path/to/statusfile'
